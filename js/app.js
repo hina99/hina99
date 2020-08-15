a781c48a8f24685e51bfbdb1d85ebfbcb9d132a3
hina99 <69730450+hina99@users.noreply.github.com>
2020-08-15 23:20:34 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
     const body = document.querySelector('body')
     const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
     const value = document.querySelector('#hex-value')
-
+    body.style.backgroundColor = #000000
     button.addEventListener('click', changeHex)
 
     function changeHex(){
